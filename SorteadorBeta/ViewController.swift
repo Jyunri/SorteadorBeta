f7df5c488fe5c8903a303a031652a909db8edd6e
Jonathan Suenaga <jonathansuenaga@Mac-Air-DMA10.local>
2016-08-17 17:15:31 -0300

    init

---
+++ SorteadorBeta/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SorteadorBeta
+//
+//  Created by Jonathan Suenaga on 8/17/16.
+//  Copyright (c) 2016 Jonathan Suenaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBAction func pressed(sender: UIButton) {
+    }
+
+}
+
